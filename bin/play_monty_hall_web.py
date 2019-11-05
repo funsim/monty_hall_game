52929fabdd53903e010b220da76ff1e40aeb1866
Simon Funke <simon.funke@gmail.com>
2019-11-05 19:02:04 +0100

    Fix port for heroku

--- bin/play_monty_hall_web.py
+++ bin/play_monty_hall_web.py
@@ -61,5 +61,5 @@ def statistics():
     return "<h1>Statistics</h1>{}".format(stats)
 
 if __name__ == "__main__":
-     port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 5000))
     app.run(host="0.0.0.0", port=port)
